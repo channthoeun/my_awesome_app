3614aa0e034ca7a189ee8adde6e25eeff098d04a
Channthoeun Ken <channthoeun@gmail.com>
2025-06-28 16:51:39 +0700

    Updated auth service

--- lib/api/auth_service.dart
+++ lib/api/auth_service.dart
@@ -1,15 +1,32 @@
-class AuthService {
-  // Simulate a login API call
-  Future<String> login(String email, String password) async {
-    // Simulate network delay
-    await Future.delayed(const Duration(seconds: 1));
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:my_awesome_app/utils/app_config.dart';
 
-    if (email == 'test@test.com' && password == 'password') {
-      // On success, return a fake token
-      return 'fake_auth_token_for_user_123';
-    } else {
-      // On failure, throw an error
-      throw Exception('Invalid credentials');
+class AuthService {
+  final String _baseUrl = AppConfig.baseUrl;
+
+  Future<String> login(String username, String password) async {
+    final url = Uri.parse('$_baseUrl/auth/token');
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({'username': username, 'password': password}),
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final accessToken = data['access_token'];
+        if (accessToken != null) {
+          return accessToken;
+        } else {
+          throw Exception('Access token not found in response.');
+        }
+      } else {
+        throw Exception('Failed to login. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('An error occurred during login.');
     }
   }
 }
