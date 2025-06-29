23ff4bb194ebae2d03c8bdc2cc3f8a283842b801
Channthoeun Ken <channthoeun@gmail.com>
2025-06-29 12:44:41 +0700

    Added QR scanner screen

---
+++ lib/screens/qr_scanner_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class QrScannerScreen extends StatelessWidget {
+  const QrScannerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Scan QR Code')),
+      body: MobileScanner(
+        // The onDetect callback is where the magic happens
+        onDetect: (capture) {
+          final List<Barcode> barcodes = capture.barcodes;
+          if (barcodes.isNotEmpty) {
+            final String? scannedValue = barcodes.first.rawValue;
+            if (scannedValue != null) {
+              // Pop the screen and return the scanned value
+              Navigator.of(context).pop(scannedValue);
+            }
+          }
+        },
+      ),
+    );
+  }
+}
